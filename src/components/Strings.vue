5a1d2c0437fe058d870b01a1a1dc81bb61428d3d
rzett <rzett@software-crafter.com>
2020-08-24 20:33:04 +0200

    First Commit

---
+++ src/components/Strings.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <li v-bind:key="string.id" v-for="string in strings"></li>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Strings",
+    props: ["strings"]
+}
+</script>
+
+<style scoped>
+
+</style>
